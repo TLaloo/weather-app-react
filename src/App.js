ea713d33a39a3876831719a6412e0a1426aa76da
Tricia Laloo <tricia_laloo@quadra.ca>
2023-02-21 09:33:26 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,15 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Tricia Laloo and is{" "}
+        <a href="https://github.com/TLaloo/weather-app-react" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
